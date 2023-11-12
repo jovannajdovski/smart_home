49e1ffa61f507a3a47d2ad7a5f5aa913b82597d1
jovannajdovski <j.najdovski@gmail.com>
2023-11-12 15:02:09 +0100

    UDS sensor and simulators added

--- PI1/PI1_controller.py
+++ PI1/PI1_controller.py
@@ -2,6 +2,7 @@
 import threading
 from settings import load_settings
 from components.dht import run_dht
+from components.uds import run_uds
 import time
 
 try:
@@ -12,19 +13,21 @@ except:
 
 
 if __name__ == "__main__":
-    print('Starting PI1 controller')
+    print('\nStarting PI1 controller\n')
     settings = load_settings()
     threads = []
     stop_event = threading.Event()
     try:
         rdht1_settings = settings['RDHT1']
         rdht2_settings = settings['RDHT2']
+        dus1_settings = settings['DUS1']
         run_dht(rdht1_settings, threads, stop_event)
         run_dht(rdht2_settings, threads, stop_event)
+        run_uds(dus1_settings, threads, stop_event)
         while True:
             time.sleep(1)
 
     except KeyboardInterrupt:
-        print('Stopping PI1 controller')
+        print('\nStopping PI1 controller\n')
         for t in threads:
             stop_event.set()
