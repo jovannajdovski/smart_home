a4b419f4a0f5182ad551fdfe22498aa9632549c3
Nikola1709 <savickosovska@gmail.com>
2023-11-13 23:28:55 +0100

    implemented membrane switch simulator and sensor

---
+++ PI1/simulators/membrane_switch.py
@@ -0,0 +1,21 @@
+import time
+import random
+
+def generate_values(probability=0.2):
+    keys=['0','1','2','3','4','5','6','7','8','9','*','0','#','A','B','C','D']
+
+    while True:
+        r=random.random()
+        if r < probability:
+            yield keys[round(len(keys)*r/probability)]
+        else:
+            yield None
+
+
+def run_membrane_switch_simulator(id, delay, callback, stop_event):
+    for pressed_key in generate_values():
+        time.sleep(delay)
+        if pressed_key is not None:
+            callback(pressed_key, id)
+        if stop_event.is_set():
+            break
