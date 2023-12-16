b78e95558031bb8de72cd9645f4476d174ceeec2
Nikola1709 <savickosovska@gmail.com>
2023-12-16 23:29:04 +0100

    Docker compose setup. Server initialized

--- PI1/settings.py
+++ PI1/settings.py
@@ -1,6 +1,6 @@
 import json
 import os
-def load_settings(filePath='settings.json'):
+def load_settings(filePath='..\config\settings.json'):
     script_dir = os.path.dirname(__file__)
     
     # Create the absolute path to the settings.json file
