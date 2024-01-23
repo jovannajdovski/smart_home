d2179afd780ed62acd78ede2831d49fd61d24d6f
jovannajdovski <j.najdovski@gmail.com>
2024-01-23 23:57:43 +0100

    Clock alarm song added

--- PI/actuators/buzzer.py
+++ PI/actuators/buzzer.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 import random
+import threading
+from actuators.music.notes import notes, load_song
 
+song_stop_event = None
 
 class Buzzer(object):
     def __init__(self, id, pin, pitch):
@@ -38,17 +41,47 @@ class Buzzer(object):
     def turn_off(self):
         GPIO.output(self.pin, False)
 
+    def buzz_note(self, pitch, duration):
+        period = 1.0/pitch
+        delay = period/2.0
+        cycles = int(duration * pitch)
+
+        for _ in range(cycles):
+            GPIO.output(self.pin, GPIO.HIGH)
+            time.sleep(delay)
+            GPIO.output(self.pin, GPIO.LOW)
+            time.sleep(delay)
+
+def play_song(buzzer, song, song_stop_event, duration=1, pause=1):
+    for note in song:
+        if song_stop_event.is_set():
+            break
+        buzzer.buzz_note(notes[note['note']], note['duration'] * duration)
+        if song_stop_event.is_set():
+            break
+        time.sleep(note['duration'] * duration*pause)
+
 def run_buzzer_loop(buzzer, settings, delay, duration, callback, stop_event, alarm_clock_event):
     clock_alarm_started = False
+    global song_stop_event
+    song_stop_event = threading.Event()
+
     while True:
         if settings["id"] == "BB":
             if alarm_clock_event.is_set() and not clock_alarm_started:
                 clock_alarm_started = True
-                buzzer.turn_on()
+                # buzzer.turn_on()
+                song = load_song("zuta_kuca.json")
+                song_stop_event.clear()
+                song_thread = threading.Thread(target = play_song, args=(buzzer, song, song_stop_event))
+                song_thread.start()
+
                 callback("CLOCK ALARM STARTED", settings)
             elif not alarm_clock_event.is_set() and clock_alarm_started:
                 clock_alarm_started = False
-                buzzer.turn_off()
+                # buzzer.turn_off()
+                song_stop_event.set()
+
                 callback("CLOCK ALARM TURNED OFF", settings)
         if stop_event.is_set():
             buzzer.turn_off()
